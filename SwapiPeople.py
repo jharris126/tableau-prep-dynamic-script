01b6ea5fd0b0b53a7e7803bd68d331a03e07e283
Jeremy Harris <jharris@coh.org>
2021-03-23 14:06:20 -0700

    add parameter so get_schema_output only queries 1 page of swapi data

--- SwapiPeople.py
+++ SwapiPeople.py
@@ -1,17 +1,18 @@
 import requests
 import pandas as pd
 
-#gets covid data as json from api and returns dataframe, converts all columns to strings
-def get_data():
+#gets data as json from api and returns dataframe, converts all columns to strings (schema only grabs first page)
+def get_data(reason):
     url = 'https://swapi.dev/api/people'
     response = requests.get(url).json()
     next_page = response['next']
     data = response['results']
 
-    while next_page is not None:
-        response = requests.get(next_page).json()
-        data.extend(response['results'])
-        next_page = response['next']
+    if reason == 'data':
+        while next_page is not None:
+            response = requests.get(next_page).json()
+            data.extend(response['results'])
+            next_page = response['next']
 
     return pd.DataFrame(data).astype(str)
 
@@ -32,11 +33,11 @@ def convert_types(type):
 
 #make get dataframe from api call and return to prep
 def get_swapi_data(df):
-    return get_data()
+    return get_data('data')
 
 
 #get dataframe from api call and return schema to prep
 def get_output_schema():
-    cols_dict = get_data().dtypes.to_dict()
+    cols_dict = get_data('schema').dtypes.to_dict()
     cols_dict = {item: convert_types(value) for (item, value) in cols_dict.items()}
     return pd.DataFrame(cols_dict)
