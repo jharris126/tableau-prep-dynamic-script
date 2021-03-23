e19ae5bdc129df4e22092c5be1c96ffd22ea11c4
Jeremy Harris <jharris@coh.org>
2021-03-23 13:28:04 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tableau-prep-dynamic-script
+ Example of a Tableau Prep flow that gathers data using a Script Task and dynamically produces a result based on data received.
---
+++ SwapiPeople.py
@@ -0,0 +1,42 @@
+import requests
+import pandas as pd
+
+#gets covid data as json from api and returns dataframe, converts all columns to strings
+def get_data():
+    url = 'https://swapi.dev/api/people'
+    response = requests.get(url).json()
+    next_page = response['next']
+    data = response['results']
+
+    while next_page is not None:
+        response = requests.get(next_page).json()
+        data.extend(response['results'])
+        next_page = response['next']
+
+    return pd.DataFrame(data).astype(str)
+
+
+#converts pandas data types to prep data types (leaves dates as string to convert in prep)
+def convert_types(type):
+    if type == 'object':
+        new_type = prep_string()
+    elif type == 'int64':
+        new_type = prep_int()
+    elif type == 'float64':
+        new_type = prep_decimal()
+    else:
+        new_type = prep_string()
+
+    return new_type
+
+
+#make get dataframe from api call and return to prep
+def get_swapi_data(df):
+    return get_data()
+
+
+#get dataframe from api call and return schema to prep
+def get_output_schema():
+    cols_dict = get_data().dtypes.to_dict()
+    cols_dict = {item: convert_types(value) for (item, value) in cols_dict.items()}
+    return pd.DataFrame(cols_dict)
